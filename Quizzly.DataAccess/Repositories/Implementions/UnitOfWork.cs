c6fc651509a6a405929dd879bb60c1059f16a6b6
OmarElsheikh <omarelsheikh6@gmail.com>
2025-10-04 18:51:22 +0300

    Implement UnitOfWork and IUnitOfWork interfaces
    Added the `UnitOfWork` class to manage repository instances and handle database operations asynchronously. Introduced the `IUnitOfWork` interface to define repository access and include a method for saving changes. The implementation ensures proper disposal of the database context.

---
+++ Quizzly.DataAccess/Repositories/Implementions/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using Quizzly.DataAccess.Data;
+using Quizzly.DataAccess.Entities;
+using Quizzly.DataAccess.Repositories.Interfaces;
+
+namespace Quizzly.DataAccess.Repositories.Implementions
+{
+    public class UnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+
+            Answers = new AnswerRepository(_context);
+            Choices = new ChoiceRepository(_context);
+            Questions = new QuestionRepository(_context);
+            Instructors = new InstructorRepository(_context);
+            QuizAttempts = new QuizAttemptRepository(_context);
+            Quizs = new QuizRepository(_context);
+            QuizCategorys = new QuizCategoryRepository(_context);
+            StudentInfoFields = new StudentInfoFieldRepository(_context);
+            StudentInfoResponses = new StudentInfoResponseRepository(_context);
+        }
+
+       public IAnswerRepository Answers { get; }
+       public IChoiceRepository Choices { get; }
+       public IQuestionRepository Questions { get; }
+       public IInstructorRepository Instructors { get; }
+       public IQuizAttemptRepository QuizAttempts { get; }
+       public IQuizRepository Quizs { get; }
+       public IQuizCategoryRepository QuizCategorys { get; }
+       public IStudentInfoFieldRepository StudentInfoFields { get; }
+       public IStudentInfoResponseRepository StudentInfoResponses { get; }
+
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Quizzly.DataAccess/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿namespace Quizzly.DataAccess.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+        IAnswerRepository AnswerRepository { get; }
+        IChoiceRepository ChoiceRepository { get; }
+        IQuestionRepository QuestionRepository { get; }
+        IInstructorRepository InstructorRepository { get; }
+        IQuizAttemptRepository QuizAttemptRepository { get; }
+        IQuizRepository QuizRepository { get; }
+        IQuizCategoryRepository QuizCategoryRepository { get; }
+        IStudentInfoFieldRepository StudentInfoFieldRepository { get; }
+        IStudentInfoResponseRepository StudentInfoResponseRepository { get; }
+
+        Task<int> SaveAsync();
+        //IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
+    }
+}
