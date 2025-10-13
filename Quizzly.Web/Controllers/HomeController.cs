25867df0ad10874615ea6cbf4ea844498be779e3
Ahmed Alian <anmad.alian689@gmail.com>
2025-10-13 22:16:17 +0300

    Manage user authentication and role-based redirection on Index action

--- Quizzly.Web/Controllers/HomeController.cs
+++ Quizzly.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Quizzly.DataAccess.Constants;
+using Quizzly.DataAccess.Entities;
 using Quizzly.Web.Models;
 
 namespace Quizzly.Web.Controllers
@@ -7,15 +10,38 @@ namespace Quizzly.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            if (User.Identity?.IsAuthenticated != true)
+            {
+                return RedirectToAction("Login", "Account", new { area = "Authentication" });
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Account", new { area = "Authentication" });
+            }
+
+            // Redirect based on user role
+            if (await _userManager.IsInRoleAsync(user, AppRoles.Instructor))
+            {
+                return RedirectToAction("Index", "Dashboard", new { area = "Instructor" });
+            }
+            else if (await _userManager.IsInRoleAsync(user, AppRoles.Student))
+            {
+                return RedirectToAction("Index", "Dashboard", new { area = "Student" });
+            }
+
+            return RedirectToAction("Login", "Account", new { area = "Authentication" });
         }
 
         public IActionResult Privacy()
